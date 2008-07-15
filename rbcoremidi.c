2fa0ef8d70d5e97f7376875c9fd055cde4687889
Markus Prinz <markus.prinz@nuclearsquid.com>
2008-07-15 13:32:58 +0200

    Initial checkin

---
+++ rbcoremidi.c
@@ -0,0 +1,18 @@
+/*
+ *  rbcoremidi.c
+ *  rbcoremidi
+ *
+ *  Created by cypher on 15.07.08.
+ *  Copyright 2008 Nuclear Squid. All rights reserved.
+ *
+ */
+
+#include <ruby.h>
+
+
+VALUE crbCoreMidi;
+
+void Init_rbcoremidi (void)
+{
+  // Add the initialization code of your module here.
+}
