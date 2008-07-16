e1f21893cedcb02fc76415e2bb95b3d0c06d1abf
Markus Prinz <markus.prinz@nuclearsquid.com>
2008-07-16 22:38:01 +0200

    Add best guess at a method that returns an array with the names of all sources

--- rbcoremidi.c
+++ rbcoremidi.c
@@ -18,6 +18,28 @@ static void RbMIDIReadProc(const MIDIPacketList* packetList, void* readProcRefCo
     
 }
 
+static VALUE t_sources(VALUE self)
+{    
+    int number_of_sources = MIDIGetNumberOfSources();
+    
+    VALUE source_ary = rb_ary_new2(number_of_sources);
+    
+    for(int idx = 0; idx < number_of_sources; ++idx)
+    {
+        MIDIEndpointRef src = MIDIGetSource(idx);
+        CFStringRef pname;
+        char name[64];
+        
+        MIDIObjectGetStringProperty(src, kMIDIPropertyName, pname);
+        CFStringGetCString(pname, name, sizeof(name), 0);
+        CFRelease(pname);
+        
+        rb_ary_push(source_ary, rb_str_new2(name));
+    }
+    
+    return source_ary;
+}
+
 void Init_rbcoremidi (void)
 {
   // Add the initialization code of your module here.
