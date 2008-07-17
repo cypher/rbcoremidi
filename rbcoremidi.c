949293f5bfd24fc1fb19d134e670a9cc86c991da
Markus Prinz <markus.prinz@nuclearsquid.com>
2008-07-17 16:30:50 +0200

    Add t_create_input_port

--- rbcoremidi.c
+++ rbcoremidi.c
@@ -10,11 +10,28 @@
 VALUE callback_proc = Qnil;
 
 MIDIPortRef inPort = NULL;
-MIDIClientRef client = NULL;
+MIDIClientRef midi_client = NULL;
 
+// The callback function that we'll eventually supply to MIDIInputPortCreate
 static void RbMIDIReadProc(const MIDIPacketList* packetList, void* readProcRefCon, void* srcConnRefCon)
 {
     
+}
+
+// Create a new Input Port and saves the Ruby Callback proc.
+static VALUE t_create_input_port(VALUE self, VALUE client_name, VALUE port_name, VALUE proc)
+{    
+    CFStringRef client_str = CFStringCreateWithCString(kCFAllocatorDefault, RSTRING(client_name)->ptr, kCFStringEncodingASCII);
+    MIDIClientCreate(client_str, NULL, NULL, &midi_client);
+    CFRelease(client_str);
+    
+    CFStringRef port_str = CFStringCreateWithCString(kCFAllocatorDefault, RSTRING(port_name)->ptr, kCFStringEncodingASCII);
+    MIDIInputPortCreate(midi_client, port_str, RbMIDIReadProc, NULL, &inPort);
+    CFRelease(port_str);
+    
+    callback_proc = proc;
+    
+    return Qtrue;
 }
 
 static VALUE t_sources(VALUE self)
@@ -53,6 +70,7 @@ void Init_rbcoremidi (void)
     cCoreMIDI = rb_define_class("CoreMIDI", rb_cObject);
     mCoreMIDIAPI = rb_define_module_under(cCoreMIDI, "API");
     
+    rb_define_method(mCoreMIDIAPI, "create_input_port", t_create_input_port, 3);
     rb_define_method(mCoreMIDIAPI, "get_sources", t_get_sources, 0);
     rb_define_method(mCoreMIDIAPI, "get_num_sources", t_get_num_sources, 0);
 }
