de829d9482847a8319552ef6a595f3777242c404
Markus Prinz <markus.prinz@nuclearsquid.com>
2008-07-17 17:37:16 +0200

    Update ruby layer

--- rbcoremidi.rb
+++ rbcoremidi.rb
@@ -2,16 +2,15 @@
 require 'rbcoremidi.bundle'
 
 module CoreMIDI
-
-  def sources
-    API.sources
+  def self.sources
+    API.get_sources
   end
   
-  def number_of_sources
+  def self.number_of_sources
     API.get_num_sources
   end
   
-  def create_input_port(client_name, port_name, &proc)
+  def self.create_input_port(client_name, port_name, &proc)
     # AFAIK this is the only way to pass the proc to a C function
     API.create_input_port(client_name, port_name, proc)
   end
