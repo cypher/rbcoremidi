15096ca71fa5cad7dc615e8861ef1f915fd2c632
Chris Lloyd <christopher.lloyd@gmail.com>
2008-10-20 10:00:27 +1100

    Moved C extension to ext directory.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,4 +1,5 @@
 require 'mkmf'
 $CPPFLAGS += " -I/Developer/SDKs/MacOSX10.5.sdk/System/Library/Frameworks/CoreMIDI.framework/Headers"
 $LDFLAGS += " -framework CoreMIDI"
+
 create_makefile('rbcoremidi')
