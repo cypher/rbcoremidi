03200d79f2e69fb51ae7c8c44510d1665b5adc6c
Xavier Shay <xavier@rhnh.net>
2008-10-20 20:05:53 +1030

    Change extconf.rb to use a symlink to the current version of the CoreMIDI framework. This is what midiator does and seems to make a lot more sense than tying it to 10.5

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,5 +1,5 @@
 require 'mkmf'
-$CPPFLAGS += " -I/Developer/SDKs/MacOSX10.5.sdk/System/Library/Frameworks/CoreMIDI.framework/Headers"
+$CPPFLAGS += " -I/System/Library/Frameworks/CoreMIDI.framework/Versions/Current/Headers"
 $LDFLAGS += " -framework CoreMIDI"
 
 create_makefile('rbcoremidi')
